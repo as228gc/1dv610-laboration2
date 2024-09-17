1adb530dfcc8fdb23404e564e159e55dc6ae2328
Alex Söderberg <as228gc@student.lnu.se>
2024-09-17 18:18:58 +0200

    Create Transaction class and category validation method

---
+++ src/modules/Transaction/Transaction.ts
@@ -0,0 +1,58 @@
+/**
+ * @author Alex Söderberg <as228gc@student.lnu.se>
+ */
+import { ExpenseCategories } from "../../enums/ExpenseCategories"
+import { IncomeCategories } from "../../enums/IncomeCategories"
+import { TransactionTypes } from "../../enums/TransactionTypes"
+
+/**
+ * Represents a transaction.
+ * 
+ */
+export class Transaction {
+  #date: Date
+  #amount: number
+  #category: ExpenseCategories | IncomeCategories
+  #type: TransactionTypes
+
+  /**
+   * 
+   * @param date
+   * @param {number} amount - The amount of money.
+   * @param {TransactionTypes} type - Income or Expense. 
+   * @param {ExpenseCategories | IncomeCategories} category - Either a expense-, or income category.
+   * @throws {error}
+   */
+  constructor (
+    date: Date,
+    amount: number,
+    type: TransactionTypes,
+    category: ExpenseCategories | IncomeCategories ) {
+
+    this.#date = date
+    this.#amount = amount
+    this.#category = category
+    this.#type = type
+
+    // Check if the category is valid, with the given transaction type.
+    this.validateCategory(this.#category, this.#type)
+  }
+
+  /**
+   * Check if the category is valid with the given transaction type.
+   * 
+   * @param {string} category - The category of income or expense.
+   * @param {TransactionTypes} type - The type of transaction.
+   * @throws {Error} - If the category is not of the right transaction type.
+   */
+  private validateCategory (category: string, type: TransactionTypes) {
+    if (type === TransactionTypes.EXPENSE 
+    && !(Object.values(ExpenseCategories).includes(category as ExpenseCategories))) {
+      throw new Error('The entered category is not a valid expense category.')
+    } else if (
+      type === TransactionTypes.INCOME
+      && !(Object.values(IncomeCategories).includes(category as IncomeCategories))){
+        throw new Error('The entered category is not a valid income category.')
+    }
+  }
+}
