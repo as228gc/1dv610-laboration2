47f88f0ae85f6f3ac14847364cb5e3e714c10b98
Alex Söderberg <as228gc@student.lnu.se>
2024-09-19 13:49:30 +0200

    Add implementation of TransactionProcessor

--- src/modules/TransactionProcessor/TransactionProcessor.ts
+++ src/modules/TransactionProcessor/TransactionProcessor.ts
@@ -1,2 +1,34 @@
+import { TransactionType } from "../../enums/TransactionType";
+import { Transaction } from "../Transaction/Transaction";
+
 export class TransactionProcessor {
+  #transactions: Array<Transaction>
+
+  constructor(transactions: Array<Transaction>) {
+    this.#transactions = transactions
+  }
+
+  addTransaction(transaction: Transaction) {
+    this.#transactions.push(transaction)
+  }
+
+  getTotalExpenses() {
+    let total: number = 0
+    for (const transaction of this.#transactions) {
+      if (transaction.getType() === TransactionType.EXPENSE) {
+        total += transaction.getAmount()
+      }
+    }
+    return total
+  }
+
+  getTotalIncome() {
+    let total: number = 0
+    for (const transaction of this.#transactions) {
+      if (transaction.getType() === TransactionType.INCOME) {
+        total += transaction.getAmount()
+      }
+    }
+    return total
+  }
 }
