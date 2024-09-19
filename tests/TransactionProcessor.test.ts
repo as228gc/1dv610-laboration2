dbb5a0e348d713643ad3b78d63807f868dee8571
Alex Söderberg <as228gc@student.lnu.se>
2024-09-19 13:49:11 +0200

    Add test cases for TransactionProcessor

---
+++ tests/TransactionProcessor.test.ts
@@ -0,0 +1,49 @@
+import { TransactionProcessor } from "../src/modules/TransactionProcessor/TransactionProcessor";
+import { Transaction } from "../src/modules/Transaction/Transaction";
+import { TransactionType } from "../src/enums/TransactionType";
+import { ExpenseCategory } from "../src/enums/ExpenseCategory";
+import { IncomeCategory } from "../src/enums/IncomeCategory";
+
+describe('TransactionProcessor class test', () => {
+  // Create a test array of transactions
+  const transactions: Array<Transaction> = []
+
+  // Create expense transactions
+  for (let i = 0; i < 5; i++) {
+    transactions.push(
+      new Transaction(
+        new Date(),
+        100,
+        TransactionType.EXPENSE,
+        ExpenseCategory.FOOD
+      )
+    )
+  }
+
+  // Create income transactions
+  for (let i = 0; i < 5; i++) {
+    transactions.push(new Transaction(
+      new Date(),
+      100,
+      TransactionType.INCOME,
+      IncomeCategory.SALARY
+    ))
+  }
+
+  it('creates a TransactionProcessor instance', () => {
+    const transactionProcessor = new TransactionProcessor(transactions)
+    expect(transactionProcessor).toBeDefined()
+  })
+
+  it('returns the total amount of expenses', () => {
+    const transactionProcessor = new TransactionProcessor(transactions)
+    let totalExpenses = transactionProcessor.getTotalExpenses()
+    expect(totalExpenses).toEqual(500)
+  })
+
+  it('returns the total amount of income', () => {
+    const transactionProcessor = new TransactionProcessor(transactions)
+    let totalIncome = transactionProcessor.getTotalIncome()
+    expect(totalIncome).toEqual(500)
+  })
+})
