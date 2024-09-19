6ebc896eee658691df08ec3249240e3c2d65f739
Alex Söderberg <as228gc@student.lnu.se>
2024-09-19 16:22:34 +0200

    Add test cases for getTransactionsByType

--- tests/TransactionProcessor.test.ts
+++ tests/TransactionProcessor.test.ts
@@ -8,7 +8,7 @@ describe('TransactionProcessor class test', () => {
   // Create a test array of transactions
   const transactions: Array<Transaction> = []
 
-  // Create expense transactions
+  // Create expense transactions in the category FOOD
   for (let i = 0; i < 5; i++) {
     transactions.push(
       new Transaction(
@@ -20,13 +20,32 @@ describe('TransactionProcessor class test', () => {
     )
   }
 
-  // Create income transactions
+  // Create income transactions in the category SALARY
   for (let i = 0; i < 5; i++) {
     transactions.push(new Transaction(
       new Date(),
       100,
       TransactionType.INCOME,
       IncomeCategory.SALARY
+    ))
+  }
+
+  // Create expense transactions in the category OTHER
+  for (let i = 0; i < 5; i++) {
+    transactions.push(new Transaction(
+      new Date(),
+      100,
+      TransactionType.EXPENSE,
+      ExpenseCategory.OTHER
+    ))
+  }
+
+  for (let i = 0; i < 5; i++) {
+    transactions.push(new Transaction(
+      new Date(),
+      100,
+      TransactionType.INCOME,
+      IncomeCategory.GIFT
     ))
   }
 
@@ -38,16 +57,16 @@ describe('TransactionProcessor class test', () => {
   it('returns the total amount of expenses', () => {
     const transactionProcessor = new TransactionProcessor(transactions)
     let totalExpenses = transactionProcessor.getTotalExpenses()
-    expect(totalExpenses).toEqual(500)
+    expect(totalExpenses).toEqual(1000)
   })
 
   it('returns the total amount of income', () => {
     const transactionProcessor = new TransactionProcessor(transactions)
     let totalIncome = transactionProcessor.getTotalIncome()
-    expect(totalIncome).toEqual(500)
+    expect(totalIncome).toEqual(1000)
   })
 
-  it('returns an array of transactions of the expense category', () => {
+  it('returns an array of transactions of the expense category FOOD', () => {
     const transactionProcessor = new TransactionProcessor(transactions)
     expect(
       transactionProcessor
@@ -57,14 +76,33 @@ describe('TransactionProcessor class test', () => {
     )
   })
 
-  it('returns an array of transactions of the income category', () => {
+  it('returns an array of transactions of the income category SALARY', () => {
     const transactionProcessor = new TransactionProcessor(transactions)
-    console.log(transactionProcessor.getTransactionsByCategory(IncomeCategory.SALARY))
     expect(
       transactionProcessor
       .getTransactionsByCategory(IncomeCategory.SALARY)
     ).toEqual(
       transactions.slice(5, 10)
+    )
+  })
+
+  it('returns an array of transactions of the transaction type EXPENSE', () => {
+    const transactionProcessor = new TransactionProcessor(transactions)
+    expect(
+      transactionProcessor
+      .getTransactionsByType(TransactionType.EXPENSE)
+    ).toEqual(
+      transactions.slice(0, 5).concat(transactions.slice(10, 15))
+    )
+  })
+
+  it('returns an array of transactions of the transaction type INCOME', () => {
+    const transactionProcessor = new TransactionProcessor(transactions)
+    expect(
+      transactionProcessor
+      .getTransactionsByType(TransactionType.INCOME)
+    ).toEqual(
+      transactions.slice(5, 10).concat(transactions.slice(15, 20))
     )
   })
 })
