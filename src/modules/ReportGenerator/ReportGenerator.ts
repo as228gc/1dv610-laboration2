8f4680b1f9cf602d0261ad20b8a953f6f7fe5ed0
Alex Söderberg <as228gc@student.lnu.se>
2024-09-25 18:46:23 +0200

    Add mapping and generateSummary method

--- src/modules/ReportGenerator/ReportGenerator.ts
+++ src/modules/ReportGenerator/ReportGenerator.ts
@@ -16,25 +16,13 @@ import { Report } from "../Report/Report";
 export class ReportGenerator {
   #processor: TransactionProcessor
 
+  /**
+   * Creates a ReportGenerator instance.
+   *
+   * @param { TransactionProcessor } processor - A transaction processor for handling transaction to be included in the report.
+   */
   constructor(processor: TransactionProcessor) {
     this.#processor = processor
-  }
-
-  /**
-   * Returns the total amount of expenses from the collection of transactions.
-   *
-   * @returns {number} - The total expenses from the array of transactions.
-   */
-  calculateExpenses(): number {
-    let total: number = 0
-
-    // For every transaction of the type EXPENSE
-    for (const transaction of this.#processor.filterByType(TransactionType.EXPENSE)) {
-      if (transaction.getType() === TransactionType.EXPENSE) {
-        total += transaction.getAmount()
-      }
-    }
-    return total
   }
 
   /**
@@ -55,6 +43,24 @@ export class ReportGenerator {
   }
 
   /**
+   * Returns the total amount of expenses from the collection of transactions.
+   *
+   * @returns {number} - The total expenses from the array of transactions.
+   */
+  calculateExpenses(): number {
+    let total: number = 0
+
+    // For every transaction of the type EXPENSE
+    for (const transaction of this.#processor.filterByType(TransactionType.EXPENSE)) {
+      if (transaction.getType() === TransactionType.EXPENSE) {
+        total += transaction.getAmount()
+      }
+    }
+    return total
+  }
+
+
+  /**
    * Returns the net balance of the transactions.
    *
    * @returns { number } The net balance.
@@ -63,24 +69,42 @@ export class ReportGenerator {
     return this.calculateIncome() - this.calculateExpenses() 
   }
 
-  // generateSummaryReport(): Report {
-  //   const report: Report = new Report(
-  //     this.calculateIncome(),
-  //     this.calculateExpenses(),
-  //     this.calculateNetBalance(),
-  //   )
-  // }
+  /**
+   * Generates a report that covers all transactions.
+   *
+   * @returns { Report } A report containing a summary of all transactions.
+   */
+  generateSummaryReport(): Report {
+    // Get the transactions with the earliest and latest date
+    const firstDate: Date = this.#processor.sortByDate()[0].getDate()
+
+    const lastDate: Date = this.#processor
+      .sortByDate()[this.#processor.getTransactions().length - 1]
+      .getDate()
+
+    const report: Report = new Report(
+      this.calculateIncome(),
+      this.calculateExpenses(),
+      this.calculateNetBalance(),
+      firstDate,
+      lastDate,
+      this.summarizeCategories().incomeByCategory,
+      this.summarizeCategories().expensesByCategory
+    )
+
+    return report
+  }
 
   // generateCategoryReport(category: IncomeCategory | ExpenseCategory): Report {
 
   // }
 
-  /**
-   * Returns a report of the transactions of the chosen type.
-   *
-   * @param {TransactionType} type - Which type of transaction the report should cover.
-   * @returns {Report} - A report of the transactions of the chosen type.
-   */
+  // /**
+  //  * Returns a report of the transactions of the chosen type.
+  //  *
+  //  * @param {TransactionType} type - Which type of transaction the report should cover.
+  //  * @returns {Report} - A report of the transactions of the chosen type.
+  //  */
   // generateTypeReport(type: TransactionType): Report {
 
   // }
@@ -88,4 +112,71 @@ export class ReportGenerator {
   // generateMonthlyReport() {
 
   // }
+
+
+  /**
+   * Returns an object containing the total amount of expenses and income for each category.
+   *
+   * @returns { any } An object containing the maps of income and expenses by category.
+   */
+  private summarizeCategories(): any  {
+    const expensesMap: Map<ExpenseCategory, number> = new Map<ExpenseCategory, number>()
+    const incomeMap: Map<IncomeCategory, number> = new Map<IncomeCategory, number>()
+  
+    // Store transactions in variable for reducing amount of code
+    const transactions = this.#processor.getTransactions()
+
+    for (const transaction of transactions) {
+      // Check if the transaction is of the type EXPENSE
+      if (transaction.getType() === TransactionType.EXPENSE) {
+  
+        // If the key, value pair already exists in the Map,
+        // add the value of the transaciton to correct key value pair
+        if (expensesMap.has(transaction.getCategory() as ExpenseCategory)) {
+  
+          // Check if the value is currently undefined, if so, set the value to 0
+          const currentValue = expensesMap.get(transaction.getCategory() as ExpenseCategory) || 0
+          expensesMap
+            .set(
+              transaction.getCategory() as ExpenseCategory,
+  
+              // Add the value of the transaction to the category.
+              currentValue + transaction.getAmount()
+            )
+        } else {
+          expensesMap
+            .set(
+              transaction.getCategory() as ExpenseCategory,
+              transaction.getAmount()
+            )
+        }
+      } else if (transaction.getType() === TransactionType.INCOME) {
+        if (incomeMap.has(transaction.getCategory() as IncomeCategory)) {
+  
+          // Check if the value is currently undefined, if so, set the value to 0
+          const currentValue = incomeMap.get(transaction.getCategory() as IncomeCategory) || 0
+          incomeMap
+            .set(
+              transaction.getCategory() as IncomeCategory,
+              
+              // Add the value of the transaction to the category.
+              currentValue + transaction.getAmount()
+            )
+        } else {
+          incomeMap
+            .set(
+              transaction.getCategory() as IncomeCategory,
+              transaction.getAmount()
+            )
+        }
+      }
+    }
+
+    const summary = {
+      "incomeByCategory": incomeMap,
+      "expensesByCategory": expensesMap
+    }
+
+    return summary
+  }
 }
