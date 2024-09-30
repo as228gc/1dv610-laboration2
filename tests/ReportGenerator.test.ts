e25b01d6b99a37e81d7facd8ed6f47e06e0bf334
Alex Söderberg <as228gc@student.lnu.se>
2024-09-30 17:38:48 +0200

    Update ReportGenerator tests

--- tests/ReportGenerator.test.ts
+++ tests/ReportGenerator.test.ts
@@ -4,22 +4,27 @@ import { TransactionType } from "../src/enums/TransactionType"
 import { Transaction } from "../src/modules/Transaction/Transaction"
 import { ExpenseCategory } from "../src/enums/ExpenseCategory"
 import { IncomeCategory } from "../src/enums/IncomeCategory"
+import { Report } from "../src/modules/Report/Report"
 
 describe('ReportGenerator class test', () => {
 
   // Create transacitons array for expenses and income, to test Map.
   const transactions: Array<Transaction> = []
+  let testValue = 100
+  let totalExpenses = 0
+  let totalIncome = 0
 
   // Create expense transactions
   for (let i = 0; i < 5; i++) {
     transactions.push(
       new Transaction(
         new Date('2024-09-23'),
-        100,
+        testValue,
         TransactionType.EXPENSE,
         ExpenseCategory.FOOD
       )
     )
+    totalExpenses += testValue
   }
 
   // Create income transactions
@@ -27,42 +32,72 @@ describe('ReportGenerator class test', () => {
     transactions.push(
       new Transaction(
         new Date('2024-09-25'),
-        100,
+        testValue,
         TransactionType.INCOME,
         IncomeCategory.SALARY
       )
     )
+    totalIncome += testValue
   }
 
+  for (let i = 0; i < 5; i++) {
+    transactions.push(
+      new Transaction(
+        new Date('2024-09-24'),
+        testValue,
+        TransactionType.EXPENSE,
+        ExpenseCategory.FOOD
+      )
+    )
+    totalExpenses += testValue
+  }
+
+
+  const processor = new TransactionProcessor(transactions)
+  const sorted = processor.sortByDate()
+  const startDate = sorted[0].getDate()
+  const endDate = sorted[sorted.length - 1].getDate()
+
   it('should create a ReportGenerator instance with valid constructor', () => {
-    const processor = new TransactionProcessor(transactions)
     const generator = new ReportGenerator(processor)
     expect(generator).toBeDefined()
   })
 
   it('returns the total amount of income', () => {
-    const processor = new TransactionProcessor(transactions)
     const generator = new ReportGenerator(processor)
-    expect(generator.calculateIncome()).toEqual(500)
+    expect(generator.calculateIncome(startDate, endDate)).toEqual(totalIncome)
   })
 
   
   it('returns the total amount of expenses', () => {
-    const processor = new TransactionProcessor(transactions)
     const generator = new ReportGenerator(processor)
-    expect(generator.calculateExpenses()).toEqual(500)
+    expect(generator.calculateExpenses(startDate, endDate)).toEqual(totalExpenses)
   })
   
   it('should generate the net balance of the transactions', () => {
-    const processor = new TransactionProcessor(transactions)
     const generator = new ReportGenerator(processor)
-    expect(generator.calculateNetBalance()).toEqual(0)
+    expect(generator.calculateNetBalance(startDate, endDate)).toEqual(totalIncome - totalExpenses)
   })
 
   it('should generate a report covering all transactions', () => {
+    const generator = new ReportGenerator(processor)
+    const report = generator.generateSummaryReport()
+    expect(report.toJSON()).toEqual(
+      {
+        totalIncome: testValue * 5,
+        totalExpenses: testValue * 10,
+        netBalance: testValue * -5,
+        startDate: new Date('2024-09-23T00:00:00.000Z'),
+        endDate: new Date('2024-09-25T00:00:00.000Z'),
+        expenseByCategory: [ { Category: 'food', TotalAmount: 1000 } ],
+        incomeByCategory: [ { Category: 'salary', TotalAmount: 500 } ]
+      }
+    )
+  })
+
+  it('should calculate total expenses of transactions in a given time span', () => {
     const processor = new TransactionProcessor(transactions)
     const generator = new ReportGenerator(processor)
-    console.log(generator.generateSummaryReport().toString())
-    expect(generator.generateSummaryReport().toString)
+    expect(generator.calculateExpenses(new Date('2024-09-24'), new Date('2024-09-24'))).toEqual(500)
   })
 })
