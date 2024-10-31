ffb1986468a7c05b304ec34b37c5002b9f4dd739
Alex Söderberg <as228gc@student.lnu.se>
2024-11-01 00:19:09 +0100

    Add tests for getters

--- tests/Report.test.ts
+++ tests/Report.test.ts
@@ -1,48 +1,10 @@
-import { ExpenseCategory } from "../src/enums/ExpenseCategory";
-import { IncomeCategory } from "../src/enums/IncomeCategory";
-import { Transaction } from "../src/modules/Transaction/Transaction";
+import { ExpenseCategory, IncomeCategory } from "../src";
+import { CategorySummaryDTO } from "../src/lib/CategorySummaryDTO";
 import { ReportGenerator } from "../src/modules/ReportGenerator/ReportGenerator";
 import { TransactionProcessor } from "../src/modules/TransactionProcessor/TransactionProcessor";
-import { ExpenseTransaction } from "../src/modules/Transaction/ExpenseTransaction";
-import { IncomeTransaction } from "../src/modules/Transaction/IncomeTransaction";
 import { mock } from "./TransactionArrayMock";
 
 describe('Report class tests', () => {
-
-  // Create transacitons array for expenses and income, to test Map.
-  // const transactions: Array<Transaction> = []
-
-  // // Create expense transactions
-  // for (let i = 0; i < 5; i++) {
-  //   transactions.push(
-  //     new ExpenseTransaction(
-  //       new Date('2024-09-30'),
-  //       100,
-  //       ExpenseCategory.FOOD
-  //     )
-  //   )
-  // }
-
-  // // Create income transactions
-  // for (let i = 0; i < 5; i++) {
-  //   transactions.push(
-  //     new IncomeTransaction(
-  //       new Date('2024-09-30'),
-  //       100,
-  //       IncomeCategory.SALARY
-  //     )
-  //   )
-  // }
-
-  // for (let i = 0; i < 5; i++) {
-  //   transactions.push(
-  //     new IncomeTransaction(
-  //       new Date('2024-09-30'),
-  //       200,
-  //       IncomeCategory.GIFT
-  //     )
-  //   )
-  // }
 
   const processor = new TransactionProcessor(mock.transactions)
   const generator = new ReportGenerator(processor)
@@ -87,5 +49,65 @@ describe('Report class tests', () => {
     Income by category:
       salary: 500
       gift: 500\n`)
+  })
+
+  it('should return the start date', () => {
+    const report = generator.generateReport()
+    expect(report.getStartDate()).toEqual(mock.startDate)
+  })
+
+  it('should return the total income', () => {
+    const report = generator.generateReport()
+    expect(report.getTotalIncome()).toEqual(mock.totalIncome)
+  })
+
+  it('should return the total expenses', () => {
+    const report = generator.generateReport()
+    expect(report.getTotalExpenses()).toEqual(mock.totalExpenses)
+  })
+
+  it('should return the net balance', () => {
+    const report = generator.generateReport()
+    expect(report.getNetBalance()).toEqual(mock.netBalance)
+  })
+
+  it('should return the end date', () => {
+    const report = generator.generateReport()
+    expect(report.getEndDate()).toEqual(mock.endDate)
+  })
+
+  it('should return the start date', () => {
+    const report = generator.generateReport()
+    expect(report.getStartDate()).toEqual(mock.startDate)
+  })
+
+  it('should return the expenses by category', () => {
+    const report = generator.generateReport()
+    const dtoArray: Array<CategorySummaryDTO> = [
+      {
+        category: ExpenseCategory.FOOD,
+        totalAmount: 500
+      },
+      {
+        category: ExpenseCategory.TRANSPORT,
+        totalAmount: 500
+      }
+    ]
+    expect(report.getExpensesByCategory()).toEqual(dtoArray)
+  })
+
+  it('should return the income by category', () => {
+    const report = generator.generateReport()
+    const dtoArray: Array<CategorySummaryDTO> = [
+      {
+        category: IncomeCategory.SALARY,
+        totalAmount: 500
+      },
+      {
+        category: IncomeCategory.GIFT,
+        totalAmount: 500
+      }
+    ]
+    expect(report.getIncomeByCategory()).toEqual(dtoArray)
   })
 })
