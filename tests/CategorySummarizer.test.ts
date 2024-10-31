43b07b020d24c21445192d22febbdb1cf7687002
Alex Söderberg <as228gc@student.lnu.se>
2024-10-31 18:18:45 +0100

    Create CategorySummarizer test

---
+++ tests/CategorySummarizer.test.ts
@@ -0,0 +1,27 @@
+import { CategorySummarizer } from "../src/modules/CategorySummarizer/CategorySummarizer"
+import { mock } from "./TransactionArrayMock"
+import { SummaryDTO } from "../src/lib/SummaryDTO"
+import { ExpenseCategory, IncomeCategory } from "../src"
+
+describe('CategorySummarizer class test', () => {
+  it('should return a SummaryDTO with the categorized transactions', () => {
+    const summarizer = new CategorySummarizer()
+    const summary = summarizer.summarizeCategories(mock.transactions)
+    const incomeByCategory = new Map<IncomeCategory, number>([
+      [IncomeCategory.SALARY, 500],
+      [IncomeCategory.GIFT, 500]
+    ])
+    const expenseByCategory = new Map<ExpenseCategory, number>([
+      [ExpenseCategory.FOOD, 500],
+      [ExpenseCategory.TRANSPORT, 500]
+    ])
+
+
+    const summaryComparable: SummaryDTO = {
+      expenseByCategory,
+      incomeByCategory 
+    }
+
+    expect(summary).toEqual(summaryComparable)
+  })
+})
