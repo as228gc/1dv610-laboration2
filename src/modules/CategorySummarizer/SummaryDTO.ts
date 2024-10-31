60531243081f843e63a966cc51d4041ef48eca04
Alex Söderberg <as228gc@student.lnu.se>
2024-10-31 15:13:20 +0100

    Fix: Change interface to type for DTO

--- src/modules/CategorySummarizer/SummaryDTO.ts
+++ src/modules/CategorySummarizer/SummaryDTO.ts
@@ -1,7 +1,7 @@
 import { ExpenseCategory } from "../../enums/ExpenseCategory";
 import { IncomeCategory } from "../../enums/IncomeCategory";
 
-export interface SummaryDTO {
+export type SummaryDTO = {
   expenseByCategory: Map<ExpenseCategory, number>,
   incomeByCategory: Map<IncomeCategory, number>
 }
